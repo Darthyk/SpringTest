fd97c5b6782de3b449ce1d6b600bcf1e1957edd8
darthyk <vsazhin@wiley,com>
2022-07-27 10:19:38 +0300

    Add open api for persons

--- src/main/java/com/darthyk/springtest/controller/PersonController.java
+++ src/main/java/com/darthyk/springtest/controller/PersonController.java
@@ -4,7 +4,11 @@ import com.darthyk.springtest.dto.PersonDto;
 import com.darthyk.springtest.dto.ShortPersonDto;
 import com.darthyk.springtest.model.Person;
 import com.darthyk.springtest.service.PersonService;
-import org.springframework.data.repository.query.Param;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "person", description = "The person API")
 @RestController
 @RequestMapping("/v1/person")
 public class PersonController {
@@ -21,6 +26,16 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @Operation(summary = "Gets short person DTO", tags = "person")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the person",
+                    content = {
+                            @Content(mediaType = "application/json")
+                    }
+            )
+    })
     @GetMapping("/short/{id}")
     public ShortPersonDto getShortPersonDto(@PathVariable("id") Long id) {
         return personService.getShortPersonDtoById(id);
